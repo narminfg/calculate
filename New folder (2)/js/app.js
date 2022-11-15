ec467b251aa747084c9f089561135a02b8cd69ed
narminfg <111846836+narminfg@users.noreply.github.com>
2022-11-15 12:33:30 +0400

    first

---
+++ New folder (2)/js/app.js
@@ -0,0 +1,50 @@
+
+
+function topla(){
+    
+    var input=document.getElementById('inp1').value
+    var input1=document.getElementById('inp2').value
+    
+    var intVal = Number(input);
+    var intVal1 =  Number(input1);
+    console.log(intVal);
+    console.log(intVal1);
+    
+    document.getElementById('result').innerHTML=intVal+intVal1
+}
+function cixma(){
+    
+    var input=document.getElementById('inp1').value
+    var input1=document.getElementById('inp2').value
+    
+    var intVal = Number(input);
+    var intVal1 =  Number(input1);
+    console.log(intVal);
+    console.log(intVal1);
+    
+    document.getElementById('result').innerHTML=intVal-intVal1
+}
+function vurma(){
+    
+    var input=document.getElementById('inp1').value
+    var input1=document.getElementById('inp2').value
+    
+    var intVal = Number(input);
+    var intVal1 =  Number(input1);
+    console.log(intVal);
+    console.log(intVal1);
+    
+    document.getElementById('result').innerHTML=intVal*intVal1
+}
+function bolme(){
+    
+    var input=document.getElementById('inp1').value
+    var input1=document.getElementById('inp2').value
+    
+    var intVal = Number(input);
+    var intVal1 =  Number(input1);
+    console.log(intVal);
+    console.log(intVal1);
+    
+    document.getElementById('result').innerHTML=intVal/intVal1
+}
